75d0529e083d5e75e0b41b4249c8ff1bdb2bf79c
Hector Zhao <hector.zhao@fluxfederation.com>
2018-07-21 19:52:56 +1200

    Fix the type error of migration

--- db/migrate/20180721063528_create_offer_item_validations.rb
+++ db/migrate/20180721063528_create_offer_item_validations.rb
@@ -2,9 +2,9 @@ class CreateOfferItemValidations < ActiveRecord::Migration[5.2]
   def change
     create_table :offer_item_validations do |t|
       t.datetime :validated_at, null: false
-      t.datetime :validation_note
+      t.text :validation_note
       t.datetime :invalidated_at, null: false
-      t.datetime :invalidation_reason, null: false
+      t.text :invalidation_reason, null: false
       t.references :user, index: true, null: false
       t.references :offer, index: true, null: false
 
